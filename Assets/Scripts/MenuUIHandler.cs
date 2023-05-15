2ab8148725a7579c9575ea879e31656babfd0ef2
qimer31 <116427735+qimer31@users.noreply.github.com>
2023-05-15 19:45:17 +0300

    Brick breaker
    - new scene "Menu"
    - UI (Start & Exit buttons. Text InputField, Text "Best score"
    - UI works fine

---
+++ Assets/Scripts/MenuUIHandler.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+
+[DefaultExecutionOrder(1000)]
+public class MenuUIHandler : MonoBehaviour
+{
+
+
+    private void Start()
+    {
+
+    }
+
+    public void StartNew()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
+    }
+
+}
